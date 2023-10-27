27b4da58ff0e52445680ea0f2b38bc879dd6d56e
Kush Tiwari <tiwarikush9328@gmail.com>
2023-10-27 11:31:10 +0530

    adding diff.java

--- diff.java
+++ diff.java
@@ -1,9 +1,9 @@
 package repo1;
 
-class sum{
+class diff{
     public static void main(String[] args) {
         int a = 5;
         int b = 5;
-        System.out.println(a+b);
+        System.out.println(a-b);
     }
 }
